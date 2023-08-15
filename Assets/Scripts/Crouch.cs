6185b52bbdceffe305f3815d8de9c096882b3466
Roman Pyatnov <romaandbears@gmail.com>
2023-08-15 13:18:28 +0300

    Crouch
    Сделал полностью передвижение персонажа, приседания, прыжки.

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class Crouch : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller; 
+    [SerializeField] private float _newHeight = 1; 
+    
+    private Vector3 _newPos; 
+    private bool _check = false; 
+
+    void Update()
+    {
+        _newPos = new Vector3(transform.position.x, transform.position.y + 0.6f, transform.position.z);
+        
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.LeftControl))
+        
+        {
+            if (!_check) 
+            {
+                On(); 
+            }
+            else if (_check) 
+            {
+                Off(); 
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            if (_check) 
+            {
+                Off();
+            }
+        }
+    }
+
+    void On() 
+    {
+        _check = true; 
+        controller.height = _newHeight;
+        
+    }
+
+    void Off() // Функция "Off"
+    {
+        _check = false;
+        controller.height += 0.72f; 
+        transform.position = _newPos;  
+    }
+}
