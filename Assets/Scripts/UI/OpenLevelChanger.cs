cbacaa2364cc0bf1a045b767ff080235158dcfa8
FraerMMC <134650211+FraerMMC@users.noreply.github.com>
2023-08-21 19:15:59 +0300

    fix

--- Assets/Scripts/UI/OpenLevelChanger.cs
+++ Assets/Scripts/UI/OpenLevelChanger.cs
@@ -4,24 +4,17 @@ public class OpenLevelChanger : MonoBehaviour
 {
     [SerializeField] private GameObject levelChanger;
     [SerializeField] private GameObject mainCharacter;
+    [SerializeField] private GameObject button;
 
     private bool hide = true;
 
-    private void Update()
+    public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && hide == true)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Vector3.Distance(mainCharacter.transform.position, levelChanger.transform.position) <= 2f)
+            if (Vector3.Distance(mainCharacter.transform.position, button.transform.position) <= 2f)
             {
                 ShowLevelChanger();
-            }
-            
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && hide == false)
-        {
-            if (Vector3.Distance(mainCharacter.transform.position, levelChanger.transform.position) <= 2f)
-            {
-                HideLevelChanger();
             }
         }
     }
@@ -29,23 +22,15 @@ public class OpenLevelChanger : MonoBehaviour
 
     public void ShowLevelChanger()
     {
-        if(hide == true) 
-        {
-            hide = false;
-            levelChanger.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-        }
-
+         hide = false;
+         levelChanger.SetActive(true);
+         Cursor.lockState = CursorLockMode.None;
     }
     public void HideLevelChanger()
     {
-        if (hide == false)
-        {
-            hide = true;
-            levelChanger.SetActive(false);
-            Cursor.lockState = CursorLockMode.Locked;
-
-        }
+         hide = true;
+         levelChanger.SetActive(false);
+         Cursor.lockState = CursorLockMode.Locked;
 
     }
 
